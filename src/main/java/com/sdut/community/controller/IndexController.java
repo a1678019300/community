12bf176300a51a515da021526015676350d8b680
a1678019300 <1678019300@qqcom>
2021-11-22 20:39:38 +0800

    add check cookies

--- src/main/java/com/sdut/community/controller/IndexController.java
+++ src/main/java/com/sdut/community/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.sdut.community.controller;
 
+import com.sdut.community.mapper.UserMapper;
+import com.sdut.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /*
 *created by Gao Peng on 2021/11/21
@@ -11,7 +16,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     //代表根，什么都不输入的时候默认访问下面的
     @GetMapping("/")
-    public String index(){return "index";}
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        return "index";
+    }
 }
--- src/main/java/com/sdut/community/mapper/UserMapper.java
+++ src/main/java/com/sdut/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.sdut.community.mapper;
 import com.sdut.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
@@ -10,5 +12,7 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
-
+    //将方法中的注入到sql
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
