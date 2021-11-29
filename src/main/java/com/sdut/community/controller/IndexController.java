bd93e5954467c619b55ba3ff1e6ee4e7ccffcd4d
a1678019300 <1678019300@qqcom>
2021-11-29 21:34:59 +0800

    分页功能已经实现

--- src/main/java/com/sdut/community/controller/IndexController.java
+++ src/main/java/com/sdut/community/controller/IndexController.java
@@ -1,19 +1,17 @@
 package com.sdut.community.controller;
 
-import com.sdut.community.dto.QuestionDTO;
-import com.sdut.community.mapper.QuestionMapper;
+import com.sdut.community.dto.PaginationDTO;
 import com.sdut.community.mapper.UserMapper;
-import com.sdut.community.model.Question;
 import com.sdut.community.model.User;
 import com.sdut.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /*
  *created by Gao Peng on 2021/11/21
@@ -30,7 +28,9 @@ public class IndexController {
     //代表根，什么都不输入的时候默认访问下面的
     @GetMapping("/")
     public String index(HttpServletRequest request,
-    Model model) {
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -45,8 +45,8 @@ public class IndexController {
             }
         }
 
-        List<QuestionDTO> questionList = questionService.List();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
--- src/main/java/com/sdut/community/dto/PaginationDTO.java
+++ src/main/java/com/sdut/community/dto/PaginationDTO.java
@@ -2,28 +2,72 @@ package com.sdut.community.dto;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class PageinationDTO {
-    private List<QuestionDTO> questtions;
+public class PaginationDTO {
+    private List<QuestionDTO> questions;
     private boolean showPrevious;
     private boolean showFirstPage;
     private boolean showNext;
     private boolean showEndPage;
     private Integer page;
-    private List<Integer> pages;
+    private List<Integer> pages = new ArrayList<>();
+    private Integer totalPage;
 
     public void setPagination(Integer totalCount, Integer page, Integer size) {
-        Integer totalPage = 0;
         if (totalCount % size == 0) {
             totalPage = totalCount / size;
         } else {
             totalPage = totalCount / size + 1;
         }
-    }
-    if (page==1) {
-        showFirstPage=fales;
-        showEndPagetrue;
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > totalPage) {
+            page = totalPage;
+        }
+
+        this.page = page;
+
+        pages.add(page);
+        for (int i = 1; i <= 3; i++) {
+            if (page - i > 0) {
+                pages.add(0, page - i);
+            }
+            if (page + i <= totalPage) {
+                pages.add(page + i);
+            }
+        }
+
+
+        //是否展示上一页
+        if (page == 1) {
+            showPrevious = false;
+        } else {
+            showPrevious = true;
+        }
+
+        //是否展示下一页
+        if (page == totalPage) {
+            showNext = false;
+        } else {
+            showNext = true;
+        }
+
+        //是否展示第一页
+        if (page == 1) {
+            showFirstPage = false;
+        } else {
+            showFirstPage = true;
+        }
+
+        //是否展示最后一页
+        if (page == totalPage) {
+            showEndPage = false;
+        } else {
+            showEndPage = true;
+        }
     }
 }
